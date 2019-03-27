1a84806e1d4b5ce06d2f4cddd5b7cef52c67316e
Andrealoag <andrea.lopez.agurto@gmail.com>
2019-03-27 16:08:14 -0500

    Función filtrado en consola

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const buttonAZ = document.getElementById("a-z");
 const buttonZA = document.getElementById("z-a");
+const Fuego = document.getElementById("Fire");
 const firstPage = document.getElementById("pokemones");
 const allDataPokemon = POKEMON.pokemon;
 const namePokemon = [];
 const arrAllDataPokemon = Object.values(allDataPokemon);
 
+//PINTAR TODOS LOS POKEMONES
 const showPokemonList = (arrAllDataPokemon) => {
   for (let i=0; i<allDataPokemon.length; i++) {
     firstPage.innerHTML += `
@@ -19,42 +21,17 @@ const showPokemonList = (arrAllDataPokemon) => {
 }
 showPokemonList(arrAllDataPokemon); 
 
-buttonAZ.addEventListener("click", function() {
+//ORDENAR DE A-Z
+buttonAZ.addEventListener("click",function(){
   firstPage.innerHTML = '';
-  arrAllDataPokemon.sort(function(a,b){
-    if(a.name > b.name)
-      return 1;
-    if(a.name < b.name)
-      return -1;
-    return 0;
-  }); 
-  for (let i=0; i<arrAllDataPokemon.length; i++) {
-    firstPage.innerHTML += `
-      <div class="colorPokemon" >
-        <figure>
-          <p>Nombre: ${arrAllDataPokemon[i].name}</p>
-        </figure>
-        <img src= "${arrAllDataPokemon[i].img}">
-      </div>`;
-  }
+showPokemonList(arrAllDataPokemon.sort(order)); 
 });
 
+
+//ORDENAR DE Z-A
 buttonZA.addEventListener("click",function(){
   firstPage.innerHTML = '';
-  arrAllDataPokemon.sort(function(a,b){
-    if(a.name > b.name)
-      return -1;
-    if(a.name < b.name)
-      return 1;
-    return 0;
-  });  
-  for (let i=0; i<arrAllDataPokemon.length; i++) {
-    firstPage.innerHTML += `
-      <div class="colorPokemon" >
-        <figure>
-          <p>Nombre: ${arrAllDataPokemon[i].name}</p>
-        </figure>
-        <img src= "${arrAllDataPokemon[i].img}">
-      </div>`;
-  }
+  showPokemonList(arrAllDataPokemon.sort(order).reverse());
 });
+ 
+
